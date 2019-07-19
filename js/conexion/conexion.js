393137f3e54d945a370659c84762bf2a96ec1793
joaquin <joakincortez8@gmail.com>
2019-07-19 17:44:48 -0400

    joaquin

---
+++ js/conexion/conexion.js
@@ -0,0 +1,52 @@
+class Conexion{
+
+    async get(url){
+        const areasUrl = await fetch(url);
+        const area  = await areasUrl.json();
+        return area;
+    }
+
+
+    async post(json, url) {
+        const  item =  await JSON.stringify(json);
+        console.log(item);
+        console.log(url);
+        
+        const res = await fetch(url, {
+            method: 'POST',
+            body: item,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = res.json();
+        console.log({data})
+        return data;
+      
+
+    }
+
+    async put(json, url) {
+        const  item =  await JSON.stringify(json);
+        console.log(item);
+        console.log(url);
+        
+        const res = await fetch(url, {
+            method: 'PUT',
+            body: item,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = res.json();
+        console.log({data})
+        return data;
+
+    }
+
+
+    getUrl() {
+        return `http://localhost:9000/api/`;
+    }
+    
+}
